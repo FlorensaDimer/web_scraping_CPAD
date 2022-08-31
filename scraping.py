a5de59e8759f0320370be7039317e9dc8eca8264
guimsk <guimullerk@gmail.com>
2022-08-31 17:54:10 -0300

    .

--- scraping.py
+++ scraping.py
@@ -2,6 +2,7 @@
 import urllib.request
 import re
 from bs4 import BeautifulSoup
+import csv
 
 # %%
 url_download = """http://127.0.0.1:8000/places/default/sitemap.xml"""
@@ -30,18 +31,46 @@ response_encoding = response.headers.get_content_charset()
 html = response.read().decode('ascii')
 
 # %%
-soup = BeautifulSoup(html, 'html5lib')
+soup = BeautifulSoup(html, 'html.parser')
 # %%
 tds = soup.find_all(class_="w2p_fw") #find all faz uma lista
 
 print(tds[0].img['src']) #acessando a primeira aparição da tag td, acessando a tag filha dele e pegando o conteúdo do atributo
 
-for item in tds[1:13]:
+for item in tds[1:14]: 
+#Mostra todos as informações da pagina
     print(item.string)
 
-print(tds[14].div.descendants)
+# print(tds[14].div.contents)
 
-# %%gggg
+
+#============
+listaVizinhos = str(tds[14].div.contents).split(',')
+#Pega todo o content e separa cada </a>
+
+print('separar')
+#Apenas para localizar-me
+
+# print(listaVizinhos[-1])
+
+
+for i in range(1,len(listaVizinhos)):
+#Pega apenas a sigla localizada no link
+    if i == 1:
+        neighbours = listaVizinhos[i][-7:-5]+','
+    elif i == (len(listaVizinhos)-1):
+        neighbours = neighbours+' '+listaVizinhos[i][-8:-6]
+        #Pega diferente devido ao caracter ] presente no ultimo da lista
+    else:
+        neighbours = neighbours+' '+listaVizinhos[i][-7:-5]+','
+        
+        
+print (neighbours)
+#============
+
+
+
+# %%
 conteudo = {
     "National Flag":tds[0].img['src'],
     "Area":tds[1].stripped_strings,
@@ -56,6 +85,7 @@ conteudo = {
     "Phone":tds[10].stripped_strings,
     "Postal Code Format":tds[11].stripped_strings,
     "Postal Code Regex":tds[12].stripped_strings,
-    "Neighbours":tds[13].descend
+    "Neighbours": neighbours
 
 }
+# %%
