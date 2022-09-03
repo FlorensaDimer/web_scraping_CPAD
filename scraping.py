0728edc2d8b54b920229b869b793d21ee088da6e
Guilheme M. S. Klauberg <guimullerk@gmail.com>
2022-09-02 22:00:56 -0300

    dia 02/09

--- scraping.py
+++ scraping.py
@@ -3,6 +3,7 @@ import urllib.request
 import re
 from bs4 import BeautifulSoup
 
+
 url = 'https://www.imdb.com/chart/top/'
 #Url dos 250
 request = urllib.request.Request(url)
@@ -27,14 +28,10 @@ filmes = soup.find_all(class_="titleColumn")
 #Encontrao conteudo do "soup" usando parametros
 #Encontrou todos os links dos filmes, cria uma lista
 
-for item in filmes:
-#Passa por todos os filmes e pega as partes a[href]
-    print(item.a['href'])
-
-for i in range(1,5):
+for i in range(0,1):
 #Entra no url de cada filme separado para coletar dados
-
-    url = "https://www.imdb.com"+str(item.a['href'])
+    
+    url = "https://www.imdb.com"+ str(filmes[i].a["href"])
     #Url base("https://www.imdb.com") + caminho obtido pelo link (str(item.a['href']))
     #Url dos 250
     
@@ -55,27 +52,48 @@ for i in range(1,5):
 
     soup = BeautifulSoup(response_content, 'html.parser')
     #Processo de analisar um texto para determinar sua estrutura lógica
-
-    titulo = soup.find_all(class_="sc-b73cd867-0 fbOhB")
-    #Encontrao conteudo do "soup" usando parametros
     
-    anoLancamento = soup.find_all(class_="ipc-inline-list__item")
-    #Encontrao conteudo do "soup" usando parametros
+    # titulo = soup.find(class_="sc-b73cd867-0 eKrKux")
+    # #Encontrao conteudo do "soup" usando parametros, find busca somente o 1°
+    # print("titulo")
+    # print(titulo.string)
+        
+    # anoLancamento = soup.find(class_="sc-8c396aa2-2 itZqyK")
+    # #Encontrao conteudo do "soup" usando parametros, find busca somente o 1°
+    # print("anoLancamento")
+    # print(anoLancamento.string)
     
-    urlPoster = soup.find_all(class_="ipc-lockup-overlay__screen")
-    #Encontrao conteudo do "soup" usando parametros
-    
-    imagemPoster = soup.find_all(class_="ipc-lockup-overlay__screen")
+    # urlPoster = soup.find(class_="ipc-lockup-overlay ipc-focusable")
+    # #Encontrao conteudo do "soup" usando parametros
+    # print("urlPoster")
+    # print(urlPoster["href"])
+ 
+    imagemPoster = soup.find(class_="ipc-lockup-overlay ipc-focusable")
     #Encontrao conteudo do "soup" usando parametros Conferir+++++++
+    print("imagemPoster")
+    print(imagemPoster["href"])
     
-    notaImdb = soup.find_all(class_="sc-7ab21ed2-1 jGRxWM")
-    #Encontrao conteudo do "soup" usando parametros
+    # notaImdb = soup.find(class_="sc-7ab21ed2-1 jGRxWM")
+    # #Encontrao conteudo do "soup" usando parametros
+    # print("notaImdb")
+    # print(notaImdb.string)
 
-    listaGeneros = soup.find_all(class_="ipc-inline-list__item")
-    #Encontrao conteudo do "soup" usando parametros
+    # listaGeneros = soup.find_all(href="/name/nm0001104/?ref_=tt_cl_dr_1")
+    # #Encontrao conteudo do "soup" usando parametros
+    # print("listaGeneros")
     
-    listaDiretores = soup.find_all(class_="ipc-metadata-list-item__list-content-item ipc-metadata-list-item__list-content-item--link")
-    #Encontrao conteudo do "soup" usando parametros Conferir+++++++
-# tds = soup.find_all(class_="w2p_fw") #find all faz uma lista
+    # for item in listaGeneros:
+    #     #Passa por todos os filmes e pega as partes a[href]
+    #     print(item.string)
+        
+    # listaDiretores = soup.find_all()
+    # #Encontrao conteudo do "soup" usando parametros Conferir+++++++
+    # # tds = soup.find_all(class_="w2p_fw") #find all faz uma lista
+    # print("listaDiretores")
+  
+    # for item in  listaDiretores:
+    #     #Passa por todos os filmes e pega as partes a[href]
+    #     print(item.string)
+ 
 
 # %%
